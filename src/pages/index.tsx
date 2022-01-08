89fe95c49d160ec977fd8099260608a60446079d
Dinh Thien Phuc <thienphuc.bka1412@gmail.com>
2022-01-08 21:53:42 +0700

    [PhucDT] Add feather icons and convention config files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,10 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Zoom!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <p className={styles.description}>Experience now</p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
---
+++ src/utils/constant.ts
@@ -0,0 +1,20 @@
+export const ROUTES = [
+  {
+    name: "Menus",
+    route: "/menus",
+    children: [
+      {
+        name: "Accordion",
+        route: "/menus/accordion",
+      },
+      {
+        name: "Tabs",
+        route: "/menus/tabs",
+      },
+      {
+        name: "Accordion",
+        route: "/menus/accordion",
+      },
+    ],
+  },
+];

f1f0ea0c9a56c84b6bd31f89244b8dc63c162cca
Kaito Kid <thienphuc.bka1412@gmail.com>
2022-02-15 22:11:48 +0700

    [Kid] Update tabs

--- src/gallery/tabs/tabs.style.ts
+++ src/gallery/tabs/tabs.style.ts
@@ -4,20 +4,42 @@
 import styled, { css } from "styled-components";
 
 const Styled = {
-  TabLabels: styled.div`
-    overflow: hidden;
+  Tabs: styled.div``,
+  TabLabels: styled.div<{ isVertical: boolean }>`
     border: 1px solid #ccc;
     background-color: #f1f1f1;
+
+    ${({ isVertical }) =>
+      isVertical
+        ? css`
+            float: left;
+            width: 30%;
+            height: 300px;
+          `
+        : css`
+            overflow: hidden;
+          `};
   `,
-  TabLabel: styled.button<{ isActivated: boolean }>`
+  TabLabel: styled.button<{ isActivated: boolean; isVertical: boolean }>`
     background-color: inherit;
-    float: left;
     border: none;
     outline: none;
     cursor: pointer;
-    padding: 14px 16px;
     transition: 0.3s;
     font-size: 17px;
+
+    ${({ isVertical }) =>
+      isVertical
+        ? css`
+            display: block;
+            padding: 22px 16px;
+            width: 100%;
+            text-align: left;
+          `
+        : css`
+            padding: 14px 16px;
+            float: left;
+          `};
 
     &:hover {
       background-color: #ddd;
@@ -29,10 +51,22 @@ const Styled = {
         background-color: #ccc;
       `}
   `,
-  TabContent: styled.div`
-    padding: 6px 12px;
-    border: 1px solid #ccc;
-    border-top: none;
+  TabContent: styled.div<{ isVertical: boolean }>`
+    ${({ isVertical }) =>
+      isVertical
+        ? css`
+            float: left;
+            padding: 0px 12px;
+            border: 1px solid #ccc;
+            width: 70%;
+            border-left: none;
+            height: 300px;
+          `
+        : css`
+            padding: 6px 12px;
+            border: 1px solid #ccc;
+            border-top: none;
+          `};
   `,
 };
 
--- src/gallery/tabs/tabs.tsx
+++ src/gallery/tabs/tabs.tsx
@@ -1,4 +1,6 @@
-/* GALLERY COMPONENT: TABS - https://www.w3schools.com/howto/howto_js_tabs.asp
+/* GALLERY COMPONENT: TABS
+      https://www.w3schools.com/howto/howto_js_tabs.asp, 
+      https://www.w3schools.com/howto/howto_js_vertical_tabs.asp
    ========================================================================== */
 
 import { ReactNode, useCallback, useMemo, useState } from "react";
@@ -15,9 +17,10 @@ interface IPanel {
 interface IProps {
   panels: IPanel[];
   className?: string;
+  isVertical?: boolean;
 }
 
-const Tabs = ({ className, panels }: IProps) => {
+const Tabs = ({ className, panels, isVertical = false }: IProps) => {
   const [isActivated, setActive] = useState(0);
 
   const handleSelectTab = useCallback((index: number) => {
@@ -31,24 +34,30 @@ const Tabs = ({ className, panels }: IProps) => {
       return (
         <Styled.TabLabel
           key={panel.id}
+          isVertical={isVertical}
           isActivated={isActivated === index}
           onClick={handleSelectTab(index)}
+          className="label"
         >
           {panel.label}
         </Styled.TabLabel>
       );
     });
-  }, [handleSelectTab, isActivated, panels]);
+  }, [handleSelectTab, isActivated, isVertical, panels]);
 
   const renderTabContents = useMemo(() => {
     return panels[isActivated]?.content;
   }, [isActivated, panels]);
 
   return (
-    <div className={cx("tabs", className)}>
-      <Styled.TabLabels>{renderTabLabels}</Styled.TabLabels>
-      <Styled.TabContent>{renderTabContents}</Styled.TabContent>
-    </div>
+    <Styled.Tabs className={cx("tabs", className)}>
+      <Styled.TabLabels isVertical={isVertical} className="labels">
+        {renderTabLabels}
+      </Styled.TabLabels>
+      <Styled.TabContent className="content" isVertical={isVertical}>
+        {renderTabContents}
+      </Styled.TabContent>
+    </Styled.Tabs>
   );
 };
 
